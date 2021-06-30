793c4dff272fe277c14c1ba3c195ad8c0bb30314
JohnOnt <jhontiveros28@gmail.com>
2021-06-30 14:56:58 -0400

    added index lookup

--- keyword_extract_idx.py
+++ keyword_extract_idx.py
@@ -3,31 +3,37 @@ import numpy as np
 import os
 import sys
 import glob
-
+import pickle
 
 
 # This script takes three arguments year (ex. 2020), month (ex. 3), keyterm in all caps (ex. ALCOHOL)
+
 
 #--------------------------------------------------------------------
 # Search Functions
 #--------------------------------------------------------------------
 
-def word_search(word, text, df):
-
+def word_search_ind(word, text, df, flip=False):
+    # Flip var added for the use of getting indices where the keyerm is absent
     ind = np.char.find(np.char.upper(text), word)
-    ind[ind >= 0] = 1
-    ind[ind == -1] = 0
 
-    return df.loc[ind.astype('bool')]
+    if flip:
+        ind[ind >= 0] = 0
+        ind[ind == -1] = 1
 
+    else:
+        ind[ind >= 0] = 1
+        ind[ind == -1] = 0
+
+    return df.index[ind.astype('bool')].values
 
 if __name__ == "__main__":
 
     year    = str(sys.argv[1])
     month   = str(sys.argv[2])
     keyterm = str(sys.argv[3])
+    flip = str(sys.argv[4]) == "True"
     
-
     #--------------------------------------------------------------------
     # Now has to be broken up into separate files and slowly compiled
     #--------------------------------------------------------------------
@@ -59,22 +65,21 @@ if __name__ == "__main__":
         tweet_text = day_tweets['text'].values
         tweet_text = tweet_text.astype('<U140')
 
-        day_tweets_kw = word_search(keyterm, tweet_text, day_tweets)
-        tweet_count_kw += day_tweets_kw.shape[0]
-        li.append(day_tweets_kw)
-
-    month_tweets_kw = pd.concat(li, axis=0, ignore_index=True)
+        day_inds = word_search_ind(keyterm, tweet_text, day_tweets, flip=flip)
+        # Append list of indices
+        li.append(day_inds)
 
     print('Total tweets: ', tweet_count)
     print('Total tweets kw: ', tweet_count_kw)
-    #--------------------------------------------------------------------
-    # Write Tweets 
-    #--------------------------------------------------------------------
 
-    write_dir = '/media/johnattan/LaCie/Twitter_Terms/' + keyterm + '/' 
+    # Cannot write a dataframe with different lengths for column so just pickling list object
+    write_dir = '/media/johnattan/LaCie/Twitter_Terms/indices/' 
 
     # Create directory if it does not already exist
     if not os.path.exists(write_dir):
         os.mkdir(write_dir)
 
-    month_tweets_kw.to_csv(write_dir + year + '-' + month + '.csv')
+    pickle.dump(li, open(write_dir + keyterm + "-" + month +  "-" + str(flip) + ".p", "wb" ))
+
+
+
